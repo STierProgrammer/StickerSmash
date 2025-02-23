6e826280534cb14a5d35b5c4630a74b07d6e336b
STierProgrammer <150156387+STierProgrammer@users.noreply.github.com>
2025-02-23 19:56:19 +0400

    Few changes

---
+++ components/EmojiSticker.tsx
@@ -0,0 +1,15 @@
+import { ImageSource } from "expo-image";
+import { Image, View } from "react-native";
+
+type Props = {
+    imageSize: number;
+    stickerSource: ImageSource;
+};
+
+export default function EmojiSticker({ imageSize, stickerSource}: Props) {
+    return (
+        <View style={{ top: -350 }}>
+            <Image source={stickerSource} style={{ width: imageSize, height: imageSize }} />
+        </View>
+    );
+}
